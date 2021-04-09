fa607f0255d8daf73ab2e8efce468c50cbd6b106
carson <carson@leaddyno.com>
2021-04-09 11:40:55 -0600

    initial commit

---
+++ rpn.js
@@ -0,0 +1,16 @@
+const args = process.argv;
+
+var stack = [];
+
+for (let i = 2; i < args.length; i++) {
+    const arg = args[i];
+    if (arg === "+") {
+        var val1 = stack.pop();
+        var val2 = stack.pop();
+        stack.push(val1 + val2);
+    } else {
+        stack.push(parseFloat(arg));
+    }
+}
+
+console.log("Result: " + stack[0]);
